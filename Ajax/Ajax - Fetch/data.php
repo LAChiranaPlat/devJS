d403e26f6738f8c003bf25341a36d75409ec0b6d
huarseral <fullwingchun@gmail.com>
2021-06-22 19:49:02 -0500

    Ajax - Fetch - Promise

---
+++ Ajax/Ajax - Fetch/data.php
@@ -0,0 +1,24 @@
+<?php 
+	
+
+	var_dump($_POST);
+
+	/*
+	extract($_POST);
+
+	$data=[
+		"user"=>"huarseral",
+		"key"=>"webDeveloper"
+	];
+
+	if($data["user"]==$user && $data["key"]==$key){
+		echo "{
+			'accesos':'all',
+			'token':'12369854785456963214587452155',
+			'level':'top'
+		}";
+	}else{
+		echo "Acceso denegado";
+	}
+	*/
+?>
